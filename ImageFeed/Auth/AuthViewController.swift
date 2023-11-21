b3effbf044815f4c0ac94cea75c935466b938e30
Alexander Khasianevich <hasman32320@gmail.com>
2023-11-21 03:49:05 +0300

    Add some features from 10-th sprint: - Add Web View Controller; - Add UIProgressView with KVO. - Authorization with OAuth2; - Save Bearer token.

--- ImageFeed/Auth/AuthViewController.swift
+++ ImageFeed/Auth/AuthViewController.swift
@@ -1,21 +1,34 @@
 import Foundation
 import UIKit
 
+//MARK: - AuthViewController
 final class AuthViewController: UIViewController {
     private let showWebViewSegueIdentifier = "ShowWebView"
+    
+    let oauth2Service = OAuth2Service.shared
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == showWebViewSegueIdentifier {
             guard let webViewViewController = segue.destination as? WebViewViewController
             else { fatalError("Failed to prepare for \(showWebViewSegueIdentifier)") }
             webViewViewController.delegate = self
+        } else {
+            super.prepare(for: segue, sender: sender)
         }
     }
 }
 
+// MARK: - extension WebViewViewControllerDelegate
 extension AuthViewController: WebViewViewControllerDelegate {
     func webViewViewController(_ vc: WebViewViewController, didAuthenticateWithCode code: String) {
-        //TODO: prcess code
+        oauth2Service.fetchAuthToken(code: code, completion: { result in
+            switch result {
+            case .success:
+                print("1448")
+            case .failure:
+                print("223")
+            }
+        })
     }
     
     func webViewViewControllerDidCancel(_ vc: WebViewViewController) {
